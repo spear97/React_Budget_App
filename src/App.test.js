caf9e4e0a32b636112ad8a0565a2eecabb801011
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 13:29:06 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
+/*
+    This test case checks if the 'learn react' link is rendered in the App component.
+    It uses the render method from the testing-library/react to render the App component.
+    Then, it retrieves the element containing the text 'learn react' using the getByText method.
+    Finally, it asserts that the linkElement is present in the document.
+*/
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
